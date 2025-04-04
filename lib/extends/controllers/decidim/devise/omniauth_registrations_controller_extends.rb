2afc56e0cf51fa7ed2c2a17a764f3cc9d39af342
moustachu <moustachu@opensourcepolitics.eu>
2025-04-04 15:00:42 +0200

    fix(sso): skip authenticity token with safe navigation operator

--- lib/extends/controllers/decidim/devise/omniauth_registrations_controller_extends.rb
+++ lib/extends/controllers/decidim/devise/omniauth_registrations_controller_extends.rb
@@ -5,13 +5,15 @@ module OmniauthRegistrationsControllerExtends
 
   included do
     skip_before_action :verify_authenticity_token, if: :saml_callback?
-    
+
     private
 
     def saml_callback?
-      custom_callback_origin = request.env["omniauth.strategy"]&.options&[:idp_sso_target_callback_origin]
+      custom_callback_origin = request.env["omniauth.strategy"]&.options&.[](:idp_sso_target_callback_origin)
+      Rails.logger.debug { "OmniauthRegistrationsControllerExtends::saml_callback? Custom callback origin: #{custom_callback_origin}" }
       result = request.path.end_with?("/callback") && custom_callback_origin.present? && URI.parse(request.origin).host == custom_callback_origin
-      
+      Rails.logger.debug { "OmniauthRegistrationsControllerExtends::saml_callback? Result : #{result}" }
+
       Rails.logger.info "Skip authenticity token verification for authorized origin: #{custom_callback_origin}" if result
       result
     end
