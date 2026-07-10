80e716308ab72baa278dad485c9f27d142e0cf78
Ivan Vergés <ivan@pokecode.net>
2026-07-10 14:44:27 +0200

    disable deface

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,8 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [:id]
 
+  config.deface.enabled = ENV.fetch("DEFACE_ENABLED", nil) == "true"
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",
